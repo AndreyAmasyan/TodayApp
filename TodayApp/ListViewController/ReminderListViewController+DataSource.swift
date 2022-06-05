e6446ee6cf3080fa0468e1109e7e4fffc2cd21b1
AndreyAmasyan <amasyanandrey1@gmail.com>
2022-06-05 07:25:21 -0700

    Added an id property to the Reminder model

--- TodayApp/ListViewController/ReminderListViewController+DataSource.swift
+++ TodayApp/ListViewController/ReminderListViewController+DataSource.swift
@@ -5,11 +5,11 @@ See LICENSE folder for this sample’s licensing information.
 import UIKit
 
 extension ReminderListViewController {
-    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
-    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, Reminder.ID>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, Reminder.ID>
     
-    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: String) {
-        let reminder = Reminder.sampleData[indexPath.item]
+    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: Reminder.ID) {
+        let reminder = reminders[indexPath.item]
         var contentConfiguration = cell.defaultContentConfiguration()
         contentConfiguration.text = reminder.title
         contentConfiguration.secondaryText = reminder.dueDate.dayAndTimeText
