aa8f0e522b8137f3bc7a1537a328f60ae00fef65
AndreyAmasyan <amasyanandrey1@gmail.com>
2022-06-05 17:00:41 -0700

    Updated model, made reminders identifiable, implemented target-action pattern to invoke a function when a user taps a done button, added a custom action to make the button accessible to VoiceOver

---
+++ TodayApp/ReminderDoneButton.swift
@@ -0,0 +1,13 @@
+//
+//  ReminderDoneButton.swift
+//  TodayApp
+//
+//  Created by   Andrew on 05.06.2022.
+//
+
+import UIKit
+
+class ReminderDoneButton: UIButton {
+    var id: Reminder.ID?
+    
+}
